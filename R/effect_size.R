fd48c1feecfc7e5323382a849914f6b1b50a6e92
benjaminschlegel <kontakt@benjaminschlegel.ch>
2017-06-29 10:56:15 +0200

    fixed cohens f for two way anova

--- R/effect_size.R
+++ R/effect_size.R
@@ -1,22 +1,29 @@
 cohens.f = function(aov.object){
-  SSA = summary(aov.object)[[1]][, "Sum Sq"][1]
-  SST = sum(summary(aov.object)[[1]][, "Sum Sq"])
-  return(powerAnalysis::ES.anova.oneway(data = NULL, sst = SST, ssb = SSA))
+  df = summary(aov.object)[[1]][,"Df"]
+  df = df[-length(df)]
+  MS = summary(aov.object)[[1]][,"Mean Sq"]
+  sigma2 = df/nobs(aov.object) * (MS[1:(length(MS)-1)]-MS[length(MS)])
+  
+  sigma = sqrt(sigma2)
+  sigma.epsilon = sqrt(MS[length(MS)])
+  
+  output = data.frame(cohens.f=sigma/sigma.epsilon)
+  
+  rnames = rownames(summary(aov.object)[[1]])
+  rnames = rnames[-length(rnames)]
+  
+  rownames(output) = rnames
+  return(output)
 }
 
 effect.size = function(aov.object){
-  f = cohens.f(aov.object)
-  cat("Cohen's f: ",f$f)
-  if(f$f<0.25){
-    cat(" (small effect size)\n\n")
-  }else if(f$f<0.4){
-    cat(" (medium effect)\n\n")
-  }else{
-    cat(" (large effect)\n\n")
-  }
-  cat("Eta^2:\n")
+  cat("Effect size:\n\n")
+  output = cohens.f(aov.object)
+  output$coehns.f_effect.size = ifelse(output$cohens.f<0.25,"(small effect size)",ifelse(output$cohens.f<0.4,"(medium effect)","(large effect)"))
   etasq = as.data.frame(BaylorEdPsych::EtaSq(aov.object))
-  etasq$'' = ifelse(etasq$`Eta^2`<0.06,"(small effect)",ifelse(etasq$`Eta^2`<0.14,"(medium effect)","(large effect)"))
-  print(etasq)
-  cat("\n\nOmega^2 is", f$f^2/(1-f$f^2))
+  etasq$'Eta^2 Effect Size' = ifelse(etasq$`Eta^2`<0.06,"(small effect)",ifelse(etasq$`Eta^2`<0.14,"(medium effect)","(large effect)"))
+  output = cbind(output,etasq)
+  output$'Omega^2' = output$cohens.f^2/(1-output$cohens.f^2)
+  colnames(output) = c("Cohen's f","Cohen's f Effect Size","Eta^2","Partial Eta^2","Eta^2 Effect Size","Omega^2")
+  return(output)
 }
